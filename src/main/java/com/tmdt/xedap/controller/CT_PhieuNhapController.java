95babc9655a74a9f93d73f67b267d37c2090e564
Binh Nguyen <binh.nguyen@atech88.com>
2024-07-23 14:16:51 +0700

    feat: chi tiet phieu nhap

--- src/main/java/com/tmdt/xedap/controller/CT_PhieuNhapController.java
+++ src/main/java/com/tmdt/xedap/controller/CT_PhieuNhapController.java
@@ -1,5 +1,27 @@
 package com.tmdt.xedap.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.tmdt.xedap.entity.CT_PhieuNhap;
+import com.tmdt.xedap.service.CT_PhieuNhapService;
+
+
+@CrossOrigin
+@RestController
 public class CT_PhieuNhapController {
+	
+	@Autowired
+	private CT_PhieuNhapService ctpnService;
+	
+	@GetMapping("/phieunhap/detail/{mapn}")
+	public List<CT_PhieuNhap> getDetailDH(@PathVariable("mapn") String mapn) {
+		return ctpnService.getDetailByMaPn(mapn);
+	}
 
 }
